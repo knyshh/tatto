8e47ffbec865e610480dd6d19a22d342902eec06
aknysh <aknysh@ext.cleverbridge.com>
2019-09-25 00:58:35 +0800

    add openmaps  map

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,55 @@
 import device from 'current-device'
+import mapboxgl from 'mapbox-gl';
 import 'slick-carousel';
 import "parsleyjs";
 
 $(document).ready(function(){
+
+  // const $map = $('.wrapper').hasClass('withoutmap');
+
+  if(true) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibmVyaW9nbWJoIiwiYSI6ImNqeXBzZG5kdjFlNzEzbXV3ZTlseHZiNjkifQ.imC6UjjZpNa3ZvoF0j3jAA';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ 12.36148, 51.34515],
+      zoom: 13
+    });
+
+    map.on('load', function() {
+      map.loadImage('location.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('icon', image);
+        map.addLayer({
+          "id": "points",
+          "type": "symbol",
+          "source": {
+            "type": "geojson",
+            "data": {
+              "type": "FeatureCollection",
+              "features": [{
+                "type": "Feature",
+                "geometry": {
+                  "type": "Point",
+                  "coordinates": [12.36148,51.34515]
+                }
+              }]
+            }
+          },
+          "layout": {
+            "icon-image": "icon",
+            "icon-size": 0.9
+          }
+        });
+      });
+
+
+    });
+
+    map.scrollZoom.disable();
+  }
+
+
   $('.review__list').slick({
     arrows: true,
     dots: true,
